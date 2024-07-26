5e26c6dda66235c55a5d0c7927de8e28255d822b
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-07-26 11:40:41 +0500

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Erkebulan Massainov on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
